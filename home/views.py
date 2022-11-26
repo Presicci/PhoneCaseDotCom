cfbb1f0334f04c57f4038ee364e85abaed6c1925
Presicci <mrbennjerry@gmail.com>
2022-11-26 04:03:36 -0600

    Added store searching, with proper layering with sorts

--- home/views.py
+++ home/views.py
@@ -4,19 +4,23 @@ from .models import Product
 
 
 def home_view(request):
-    query_results = Product.objects.all()
     post_result = request.POST.get("sort", "")
+    search_result = request.POST.get("search", "")
+    name_filter = request.session.get('name_filter', '')
+    if search_result != '':
+        request.session['name_filter'] = search_result
+        name_filter = request.session.get('name_filter', '')
     if post_result == 'Price - Low to high':
-        query_results = Product.objects.all().order_by('price')
+        query_results = Product.objects.filter(name__contains=name_filter).order_by('price')
     elif post_result == 'Price - High to low':
-        query_results = Product.objects.all().order_by('price').reverse()
+        query_results = Product.objects.filter(name__contains=name_filter).order_by('price').reverse()
     elif post_result == 'Availability - Low to high':
-        query_results = Product.objects.all().order_by('quantity')
+        query_results = Product.objects.filter(name__contains=name_filter).order_by('quantity')
     elif post_result == 'Availability - High to low':
-        query_results = Product.objects.all().order_by('quantity').reverse()
+        query_results = Product.objects.filter(name__contains=name_filter).order_by('quantity').reverse()
     elif post_result == 'Alphabetical - Z-A':
-        query_results = Product.objects.all().order_by('name').reverse()
+        query_results = Product.objects.filter(name__contains=name_filter).order_by('name').reverse()
     else:
-        query_results = Product.objects.all().order_by('name')
+        query_results = Product.objects.filter(name__contains=name_filter).order_by('name')
     context = {'query_results': query_results}
     return render(request, 'todo/index.html', context)
