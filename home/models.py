39d5c0dbdeb85eac09b37a517466257529b2d2d8
Presicci <mrbennjerry@gmail.com>
2022-11-26 03:08:41 -0600

    Added quantity integer field to Product model

--- home/models.py
+++ home/models.py
@@ -5,4 +5,5 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=30)
     price = models.IntegerField()
+    quantity = models.IntegerField()
     image = models.ImageField(upload_to='images/')
