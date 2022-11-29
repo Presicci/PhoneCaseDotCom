3d7657deca977d0fed21680418f1020a8bec9075
Ruben Dario Andrade <rubyd.andrade@gmail.com>
2022-11-29 03:19:23 -0600

    try 3

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'software-eng-project.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'software-eng-project.herokuapp.com', 'phonecasedotcom.herokuapp.com']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
