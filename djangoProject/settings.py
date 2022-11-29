d19ffaf345424dbc54d603b3ef0c1228e4a7db30
Ruben Dario Andrade <rubyd.andrade@gmail.com>
2022-11-29 02:59:43 -0600

    try

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -91,21 +91,21 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 #https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 # ********* THOMAS DATABASE *********** #
-# DATABASES = {
-#     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-#     #'default': {
-#     #    'ENGINE': 'django.db.backends.sqlite3',
-#     #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     #}
-# }
+DATABASES = {
+     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+     #'default': {
+     #    'ENGINE': 'django.db.backends.sqlite3',
+     #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     #}
+}
 
 # ******** DJANGO DATABASE ************ #
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
